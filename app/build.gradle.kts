12e248543cb5712815d4535acc2e9b4494054513
NiroDev <23154768+NiroDeveloper@users.noreply.github.com>
2026-04-19 18:38:57 +0200

    Publish next version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "dev.niro.cameraremote"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.1.0"
         vectorDrawables {
             useSupportLibrary = true
         }
